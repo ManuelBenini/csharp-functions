72dceb2298ea62166d65583c7549485cbccea602
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-18 17:24:07 +0200

    Terminata funzione StampaArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿#region Functions
+
+
+// using System;
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    int i = 0;
+
+    for (i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+
+    Console.Write(array[i]);
+
+    Console.Write("]");
+}
+
+int Quadrato(int numero)
+{
+    return 0;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    return new int[0];
+}
+
+int sommaElementiArray(int[] array)
+{
+    return 0;
+}
+
+#endregion
+
+int[] numbers = {2, 6, 7, 5, 3, 9};
+
+StampaArray(numbers);
